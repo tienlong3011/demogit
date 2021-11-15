0144629bf78573953d14615c7c00bb62aab04f63
Nguyễn Tiến Long <email ng k git>
2021-11-15 16:58:48 +0700

    upade

--- src/main/java/model/student.java
+++ src/main/java/model/student.java
@@ -1,4 +1,17 @@
 package model;
 
 public class student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public student(String name) {
+        this.name = name;
+    }
 }
